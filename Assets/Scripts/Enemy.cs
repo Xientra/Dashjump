ee1f914c2a535cf7f8e0d179b6b822bf98120bd7
Xientra <38855570+Xientra@users.noreply.github.com>
2020-01-15 11:54:05 +0100

    AdvancedEnemy.cs and new Version

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,46 +23,56 @@ public class Enemy : MonoBehaviour {
     void FixedUpdate() { 
         enemyRigidbody.velocity = new Vector3(speed, enemyRigidbody.velocity.y, 0);
 
-		
-		if (speed != 0)
-			CheckPlatform();
+		//CheckPlatform();
     }
 
 	public void OnDeath() {
 		Destroy(this.gameObject);
 	}
 
+	private float reflectDelay = 0.25f;
+	private float reflectTimeStamp = 0;
+
+
 	private void CheckPlatform() {
+
+		/*
+		if (Time.time < reflectTimeStamp) {
+			return;
+		}
+		else {
+			reflectTimeStamp = Time.time + reflectDelay;
+		}
+		*/
 
 		Vector3 center = col.bounds.center;
 		Vector3 extents = col.bounds.extents;
 		Vector3 size = col.bounds.size;
 
-		// forward
-		//if (Physics.Raycast(center + transform.forward * extents.z * 1.1f, -transform.up, size.y, LayerMask.NameToLayer("Ground"))) {
-		//	//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, transform.forward);
-		//	Debug.Log("forward");
-		//}
-		//// back
-		//if (Physics.Raycast(center + -transform.forward * extents.z * 1.1f, -transform.up, size.y, LayerMask.NameToLayer("Ground"))) {
-		//	//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, -transform.forward);
-		//	Debug.Log("-forward");
-		//}
-		//// right
-		//if (Physics.Raycast(center + transform.right * extents.x * 1.1f, -transform.up, size.y, LayerMask.NameToLayer("Ground"))) {
-		//	//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, transform.right);
-		//	Debug.Log("right");
-		//}
-		//// left	
-		//if (Physics.Raycast(center + -transform.right * extents.x * 1.1f, -transform.up, size.y, LayerMask.NameToLayer("Ground"))) {
-		//	//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, -transform.right);
-		//	Debug.Log("-right");
-		//}
-
-		RaycastHit hitInfo;
-		Physics.Raycast(center, -transform.up, out hitInfo, size.y, 8);
-
-		Debug.Log(hitInfo.collider.name);
+		//forward
+		if (!Physics.Raycast(center + transform.forward * extents.z * 1.1f, -transform.up, size.y)) {
+			transform.RotateAround(Vector3.up, -90);
+			//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, transform.right);
+			Debug.Log("forward");
+		}
+		// back
+		if (!Physics.Raycast(center + -transform.forward * extents.z * 1.1f, -transform.up, size.y)) {
+			transform.RotateAround(Vector3.up, -90);
+			//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, -transform.right);
+			Debug.Log("-forward");
+		}
+		// right
+		if (!Physics.Raycast(center + transform.right * extents.x * 1.1f, -transform.up, size.y)) {
+			transform.RotateAround(Vector3.up, -90);
+			//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, transform.forward);
+			Debug.Log("right");
+		}
+		// left	
+		if (!Physics.Raycast(center + -transform.right * extents.x * 1.1f, -transform.up, size.y)) {
+			transform.RotateAround(Vector3.up, -90);
+			//enemyRigidbody.velocity = Vector3.Reflect(enemyRigidbody.velocity, -transform.forward);
+			Debug.Log("-right");
+		}
 
 
 		//Vector3 startPoint = center + -transform.right * extents.x * 1.1f;
---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour {
+	public void OnDrawGizmos() { 
+		Gizmos.DrawSphere(gameObject.transform.position, 1f); 
+	}
+}
